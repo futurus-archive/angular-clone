3668f5739e02612485bbd8f96172b5477ff78af7
vu <vu@vnguyen.io>
2017-02-28 11:41:31 -0500

    first implementation p64

--- src/scope.js
+++ src/scope.js
@@ -10,22 +10,21 @@ function Scope() {
     this.$$lastDirtyWatch = null;
 }
 
-function initWatchVal() {
-}
+function initWatchVal() {}
 
-Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
     var self = this;
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn || function () {},
+        listenerFn: listenerFn || function() {},
         valueEq: !!valueEq,
         last: initWatchVal
     };
-    
+
     // unshift rather than push to support watch removal inside of $watch
-    this.$$watchers.unshift(watcher); 
+    this.$$watchers.unshift(watcher);
     this.$$lastDirtyWatch = null;
-    
+
     return function() {
         var index = self.$$watchers.indexOf(watcher);
         if (index >= 0) {
@@ -35,12 +34,12 @@ Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
     };
 };
 
-Scope.prototype.$$digestOnce = function () {
+Scope.prototype.$$digestOnce = function() {
     var self = this;
     var newVal, oldVal, dirty;
 
     // forEachRight rather than forEach to support watch removal inside of $watch
-    _.forEachRight(this.$$watchers, function (watcher) {
+    _.forEachRight(this.$$watchers, function(watcher) {
         try {
             // making sure watcher exists, see watch removing several watches test case
             if (watcher) {
@@ -65,7 +64,7 @@ Scope.prototype.$$digestOnce = function () {
     return dirty;
 };
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$digest = function() {
     var ttl = 10;
     var dirty;
     this.$$lastDirtyWatch = null;
@@ -85,13 +84,21 @@ Scope.prototype.$$areEqual = function(newVal, oldVal, valueEq) {
     } else {
         return newVal === oldVal ||
             (typeof newVal === 'number' &&
-             typeof oldVal === 'number' &&
-             isNaN(newVal) && isNaN(oldVal));
+                typeof oldVal === 'number' &&
+                isNaN(newVal) && isNaN(oldVal));
     }
 };
 
-Scope.prototype.$eval = function (expr, locals) {
+Scope.prototype.$eval = function(expr, locals) {
     return expr(this, locals);
+};
+
+Scope.prototype.$apply = function(fn) {
+    try {
+        this.$eval(fn);
+    } finally {
+        this.$digest();
+    }
 };
 
 module.exports = Scope;
